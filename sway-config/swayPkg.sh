29b15a1923c016b0e87550345204c23344a4dc82
Farah-xyz <faaraah.xyz@gmail.com>
2023-03-23 14:57:18 +0000

    added Zathura PDF Reader

--- sway-config/swayPkg.sh
+++ sway-config/swayPkg.sh
@@ -2,7 +2,7 @@
 sudo pacman -S --needed --noconfirm wayland xorg-xwayland sway swaybg swaylock swayidle waybar \
 	foot lxterminal fuzzel grim slurp swappy mako \
 	firefox qutebrowser \
-	nemo mpv yt-dlp aria2 swayimg \
+	nemo mpv yt-dlp aria2 swayimg zathura zathura-pdf-mupdf \
 	arc-gtk-theme papirus-icon-theme \
 	adobe-source-han-sans-jp-fonts ttf-dejavu ttf-font-awesome ttf-jetbrains-mono-nerd \
 	neovim fzf fd exa bat wl-clipboard trash-cli man-db brightnessctl imagemagick \
