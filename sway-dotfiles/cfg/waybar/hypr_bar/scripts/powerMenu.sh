668fddfe9ed2a0a2edae3261b27021e35ecb8637
Farah-xyz <faaraah.xyz@gmail.com>
2023-04-26 11:01:31 +0100

    added PowerMenu in hypr_bar

---
+++ sway-dotfiles/cfg/waybar/hypr_bar/scripts/powerMenu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+options=$(printf "  Power Off\n  Restart\n  Suspend\n  Hibernate\n  Log Out\n  Lock")
+
+selected=$(echo -e "$options" | fuzzel --dmenu --border-width=3 --inner-pad=10 --width=30  --line-height=30 --lines=6 -b#11121Dff -t#9399b2ff -s#2e3440ff -S#8fbcbbff -C#88c0d0ff --show-actions --terminal=foot)
+
+case "$selected" in
+	"  Power Off") poweroff ;;
+	"  Restart") reboot ;;
+	"  Suspend") systemctl suspend-then-hibernate ;;
+	"  Hibernate") systemctl hibernate ;;
+	"  Log Out") hyprctl dispatch exit ;;
+	"  Lock") swaylock -c 11121D;;
+	*) exit 1 ;;
+esac
