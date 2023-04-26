a6df0e27a39c09292476420d6c85595784cf865b
Farah-xyz <faaraah.xyz@gmail.com>
2023-04-26 21:17:07 +0100

    added Pkg jq

--- sway-config/swayPkg.sh
+++ sway-config/swayPkg.sh
@@ -6,7 +6,7 @@ sudo pacman -S --needed --noconfirm wayland xorg-xwayland sway swaybg swaylock s
 	nemo mpv yt-dlp aria2 swayimg zathura zathura-pdf-mupdf \
 	arc-gtk-theme papirus-icon-theme \
 	adobe-source-han-sans-jp-fonts ttf-dejavu ttf-font-awesome ttf-jetbrains-mono-nerd \
-	neovim fzf fd exa bat wl-clipboard trash-cli man-db brightnessctl imagemagick \
+	neovim fzf fd jq exa bat wl-clipboard trash-cli man-db brightnessctl imagemagick \
 	udiskie dosfstools ntfs-3g \
 	gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-nfs gvfs-gphoto2 \
 	unrar unzip zip \
