7436a3499d18462f15fdf79f51d4f93cd3aca733
Farah-xyz <faaraah.xyz@gmail.com>
2023-04-28 14:20:25 +0100

    change radius Border

--- sway-dotfiles/cfg/waybar/hypr_bar/scripts/powerMenu.sh
+++ sway-dotfiles/cfg/waybar/hypr_bar/scripts/powerMenu.sh
@@ -2,7 +2,7 @@
 
 options=$(printf "  Power Off\n  Restart\n  Suspend\n  Hibernate\n  Log Out\n  Lock")
 
-selected=$(echo -e "$options" | fuzzel --dmenu --border-width=3 --inner-pad=10 --width=30  --line-height=30 --lines=6 -b#11121Dff -t#9399b2ff -s#2e3440ff -S#8fbcbbff -C#88c0d0ff --show-actions --terminal=foot)
+selected=$(echo -e "$options" | fuzzel --dmenu --border-width=3 --border-radius=4 --inner-pad=10 --width=30  --line-height=30 --lines=6 -b#11121Dff -t#9399b2ff -s#2e3440ff -S#8fbcbbff -C#88c0d0ff --show-actions --terminal=foot)
 
 case "$selected" in
 	"  Power Off") poweroff ;;
