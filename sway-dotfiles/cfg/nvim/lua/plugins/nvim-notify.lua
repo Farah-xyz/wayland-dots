984496864ab00febd42395422e778249bf0e73f6
Farah-xyz <faaraah.xyz@gmail.com>
2023-07-16 22:03:09 +0100

    clean Nvim

--- sway-dotfiles/cfg/nvim/lua/plugins/nvim-notify.lua
+++ sway-dotfiles/cfg/nvim/lua/plugins/nvim-notify.lua
@@ -1,6 +1,11 @@
 -----------------------------------------------------------
 -- Notify configuration file
 -----------------------------------------------------------
-require("notify").setup({
+local status_ok, notify = pcall(require, 'notify')
+if not status_ok then
+  return
+end
+
+notify.setup({
   background_colour = "#000000",
 })
